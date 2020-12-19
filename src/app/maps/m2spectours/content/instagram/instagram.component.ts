eba4507c7119aa7bdab90f7ff78be9ab2de11f54
Adam Kariv <adam.kariv@gmail.com>
2020-12-19 20:25:39 +0200

    +ig

--- src/app/maps/m2spectours/content/instagram/instagram.component.ts
+++ src/app/maps/m2spectours/content/instagram/instagram.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spectours-content-instagram',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./instagram.component.less']
 })
 export class InstagramComponent implements OnInit {
+
+  @Input() item;
 
   constructor() { }
 
