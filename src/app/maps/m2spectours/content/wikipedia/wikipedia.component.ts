129c991ae6264064f9f5469da79b7c74f1bb6f45
Adam Kariv <adam.kariv@gmail.com>
2020-12-19 21:57:53 +0200

    +wikipedia

--- src/app/maps/m2spectours/content/wikipedia/wikipedia.component.ts
+++ src/app/maps/m2spectours/content/wikipedia/wikipedia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spectours-content-wikipedia',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wikipedia.component.less']
 })
 export class WikipediaComponent implements OnInit {
+
+  @Input() item;
 
   constructor() { }
 
