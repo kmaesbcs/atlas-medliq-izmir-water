d48a4f6188d46a7f6d90bd6c6869004c6f72f4a5
Adam Kariv <adam.kariv@gmail.com>
2020-12-20 15:57:37 +0200

    Audio + image content

--- src/app/maps/m2spectours/content/audio/audio.component.ts
+++ src/app/maps/m2spectours/content/audio/audio.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { PlayerComponent } from 'src/app/player/player.component';
 
 @Component({
   selector: 'app-spectours-content-audio',
   templateUrl: './audio.component.html',
   styleUrls: ['./audio.component.less']
 })
-export class AudioComponent implements OnInit {
+export class AudioComponent implements OnInit, OnChanges {
+
+  @Input() item;
+  @Input() active = false;
+  @ViewChild(PlayerComponent, {static: true}) player: PlayerComponent;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    if (this.player && this.player.player) {
+      if (this.active) {
+        this.player.player.play();
+      } else {
+        this.player.player.pause();
+      }  
+    }
   }
 
 }
--- src/app/maps/m2spectours/content/image/image.component.ts
+++ src/app/maps/m2spectours/content/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spectours-content-image',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image.component.less']
 })
 export class ImageComponent implements OnInit {
+
+  @Input() item;
 
   constructor() { }
 
