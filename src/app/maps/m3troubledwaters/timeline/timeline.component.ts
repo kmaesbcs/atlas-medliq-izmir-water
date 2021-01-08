94508686b1e3266f09fd967e8c5ac543ccb018a4
Adam Kariv <adam.kariv@gmail.com>
2021-01-08 22:30:16 +0200

    Scroller for timeline

--- src/app/maps/m3troubledwaters/timeline/timeline.component.ts
+++ src/app/maps/m3troubledwaters/timeline/timeline.component.ts
@@ -1,8 +1,93 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ReplaySubject } from 'rxjs';
-import { delay, first } from 'rxjs/operators';
+import { fromEvent, ReplaySubject } from 'rxjs';
+import { debounceTime, delay, filter, first, sampleTime, tap } from 'rxjs/operators';
 import { TroubledwatersService } from '../troubledwaters.service';
+
+
+class Scroller {
+  
+  startingOffset = 0;
+  startingTime = 0;
+  offset = 0;
+  done = true;
+  dragging = false;
+  dragDiff = 0;
+  scrolling = false;
+
+  constructor(private el: HTMLElement) {
+    fromEvent(el, 'wheel').pipe(
+      tap(() => { this.scrolling = true; }),
+      debounceTime(500)
+    ).subscribe(() => {
+      this.scrolling = false;
+      this.scrollEnded();
+    });
+    fromEvent(el, 'mousedown').subscribe((ev: MouseEvent) => {
+      this.dragDiff = this.el.scrollTop + ev.y;
+      this.dragging = true;
+    });
+    fromEvent(el, 'mouseup').subscribe(() => { this.dragging = false; });
+    fromEvent(el, 'mousemove').pipe(
+      filter(() => this.dragging),
+      sampleTime(33),
+      tap((ev: MouseEvent) => {
+        this.el.scrollTo({top: - ev.y + this.dragDiff});
+      }),
+      debounceTime(500),
+    ).subscribe(() => {
+      this.dragging = false;
+      this.scrollEnded();
+    });
+  }
+
+  scrollEnded() {
+    const center = this.el.getBoundingClientRect().height / 2;
+    let selected: HTMLElement = null;
+    this.el.querySelectorAll('.second').forEach((second) => {
+      if (selected === null && second.getBoundingClientRect().top > center) {
+        selected = second as HTMLElement;
+      }
+    });
+    if (selected !== null) {
+      selected.click();
+    }
+  }
+
+  scrollSmoothly(timestamp) {
+    if (this.done) {
+      return;
+    }
+    if (timestamp - this.startingTime <= 1000) {
+      let target = this.startingOffset + (this.offset - this.startingOffset) * (timestamp - this.startingTime) / 1000;
+      target = Math.ceil(target);
+      if (Math.abs(target - this.el.scrollTop) > 0) {
+        if (!this.scrolling && !this.dragging) {
+          this.el.scrollTo({top: target, behavior: 'auto'});  
+        }
+      }
+      requestAnimationFrame((x) => this.scrollSmoothly(x)); 
+    } else {
+      if (Math.abs(this.offset - this.el.scrollTop) > 0) {
+        if (!this.scrolling && !this.dragging) {
+          this.el.scrollTo({top: this.offset, behavior: 'auto'});
+        }
+      }
+      this.done = true;
+    }
+  }
+
+  update(offset) {
+    this.offset = offset;
+    this.startingTime = performance.now();
+    this.startingOffset = this.el.scrollTop;
+    if (this.done) {
+      this.done = false;
+      requestAnimationFrame((x) => this.scrollSmoothly(x));
+    }
+  }
+
+}
 
 @Component({
   selector: 'app-troubledwaters-timeline',
@@ -13,7 +98,8 @@ export class TroubledwatersTimelineComponent implements OnInit, AfterViewInit {
 
   segments: any[] = [];
   init = new ReplaySubject<void>(1);
-  offset = 0;
+  scroller: Scroller = null;
+
   @ViewChild('timeline', {static: true}) timeline: ElementRef;
 
   constructor(private troubledWaters: TroubledwatersService, private sanitizer: DomSanitizer) { }
@@ -54,12 +140,16 @@ export class TroubledwatersTimelineComponent implements OnInit, AfterViewInit {
           return;
         }
         const el = this.timeline.nativeElement as HTMLElement;
-        const firstSelector = `.segment:first-child`;
+        const firstSelector = `.segment:nth-child(2)`;
         const secondSelector = `.segment[data-segment="${segment.id}"] > .section[data-section="${timestamp.id}"] > .second[data-offset="${offset}"]`;
         const outer = el.querySelector(firstSelector);
         const inner = el.querySelector(secondSelector);
         if (inner) {
-          this.offset = outer.getBoundingClientRect().top - inner.getBoundingClientRect().top - 2;
+          const offset = inner.getBoundingClientRect().top - outer.getBoundingClientRect().top + 2;
+          if (!this.scroller) {
+            this.scroller = new Scroller(el);
+          }
+          this.scroller.update(offset);
         }
         if (this.troubledWaters.playing) {
           // this.segments.filter(x => x.id === segment.id)[0].played[offset] = true;
@@ -75,10 +165,10 @@ export class TroubledwatersTimelineComponent implements OnInit, AfterViewInit {
     return segment.played && segment.played[offset];
   }
 
-  transform() {
-    const offset = `calc(50% + (${this.offset}px))`; 
-    return `translateY(${offset})`;
-  }
+  // transform() {
+  //   const offset = `calc(50% + (${this.offset}px))`; 
+  //   return `translateY(${offset})`;
+  // }
 
   bgStyle(segment, offset) {
     if (this.played(segment, offset)) {
