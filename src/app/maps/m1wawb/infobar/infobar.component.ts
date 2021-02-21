00198a5d2b0b35eeefb25b17074ac4de15b6d6b0
Adam Kariv <adam.kariv@gmail.com>
2021-02-21 09:38:39 +0200

    Fetch about from airtable (M1)

--- src/app/maps/m1wawb/infobar/infobar.component.ts
+++ src/app/maps/m1wawb/infobar/infobar.component.ts
@@ -2,6 +2,8 @@ import { Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
+import * as marked from 'marked';
+import { WawbService } from '../wawb.service';
 @Component({
   selector: 'app-wawb-infobar',
   templateUrl: './infobar.component.html',
@@ -11,7 +13,9 @@ export class WawbInfobarComponent implements OnInit {
 
   @Output() close = new EventEmitter();
 
-  constructor() { }
+  marked = marked;
+
+  constructor(public wawb: WawbService) { }
 
   ngOnInit(): void {
   }
--- src/app/maps/m1wawb/wawb.service.ts
+++ src/app/maps/m1wawb/wawb.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { first, map, switchMap } from 'rxjs/operators';
 import { FeatureCollection, Feature, Geometry } from 'geojson';
 
 import { ApiService } from '../../api.service';
@@ -13,45 +13,67 @@ export class WawbService {
   BASE = 'app4j8ReBz6mg40Al';
 
   authorRecords = new ReplaySubject<any>(1);
+  TITLE = '';
+  SUBTITLE = '';
+  ABOUT = '';
+  MIN_ZOOM = 0
   
   constructor(private api: ApiService) {
     this.getAuthors();
   }
 
+  fetchSettings() {
+    return this.api.airtableFetch(this.BASE, 'Settings', 'website').pipe(
+      map((response: any) => {
+        const ret = {};
+        response.records.forEach((i) => {
+          ret[i.fields.key] = i.fields.value;
+        });
+        return ret;
+      })
+    );
+  }
+
   getSamples() {
-    return this.api.airtableFetch(this.BASE, 'Samples', 'website')
-        .pipe(
-          map((response: any) => response.records.map((rec) => {
-            return Object.assign(rec.fields, {id: rec.id});
-          })),
-          map((samples: any[]) => { 
+    return this.fetchSettings().pipe(
+      switchMap((settings: any) => {
+        this.TITLE = settings.title || '';
+        this.SUBTITLE = settings.subtitle || '';
+        this.ABOUT = settings.about || '';
+        this.MIN_ZOOM = parseInt(settings.min_zoom);
+        return this.api.airtableFetch(this.BASE, 'Samples', 'website');
+      }),
+      map((response: any) => response.records.map((rec) => {
+        return Object.assign(rec.fields, {id: rec.id});
+      })),
+      map((samples: any[]) => { 
+        return {
+          type: 'FeatureCollection',
+          features: samples.map((sample) => {
+            const coordinates = sample.coordinates.split(',').map((x) => parseFloat(x));
+            sample.symbol = (
+              (sample.audio_above && sample.audio_above.length) ? '' : 'no'
+            ) + 'above+' + (
+              (sample.audio_below && sample.audio_below.length) ? '' : 'no'
+            ) + 'below';
+            sample.image_above = sample.image_above ? sample.image_above[0].thumbnails.large.url: '';
+            sample.image_below = sample.image_below ? sample.image_below[0].thumbnails.large.url: '';
+            sample.audio_above = sample.audio_above ? sample.audio_above[0].url: '';
+            sample.audio_below = sample.audio_below ? sample.audio_below[0].url: '';
+            sample.author = sample.author ? sample.author.join(',') : '';
+            sample.author_credits = sample.author_credits ? sample.author_credits.join(',') : '';
             return {
-              type: 'FeatureCollection',
-              features: samples.map((sample) => {
-                const coordinates = sample.coordinates.split(',').map((x) => parseFloat(x));
-                sample.symbol = (
-                  (sample.audio_above && sample.audio_above.length) ? '' : 'no'
-                ) + 'above+' + (
-                  (sample.audio_below && sample.audio_below.length) ? '' : 'no'
-                ) + 'below';
-                sample.image_above = sample.image_above ? sample.image_above[0].thumbnails.large.url: '';
-                sample.image_below = sample.image_below ? sample.image_below[0].thumbnails.large.url: '';
-                sample.audio_above = sample.audio_above ? sample.audio_above[0].url: '';
-                sample.audio_below = sample.audio_below ? sample.audio_below[0].url: '';
-                sample.author = sample.author ? sample.author.join(',') : '';
-                sample.author_credits = sample.author_credits ? sample.author_credits.join(',') : '';
-                return {
-                  type: 'Feature',
-                  properties: sample,
-                  geometry: {
-                    type: 'Point',
-                    coordinates: [coordinates[1], coordinates[0]]
-                  } as Geometry
-                } as Feature;
-              })
-            } as FeatureCollection;
+              type: 'Feature',
+              properties: sample,
+              geometry: {
+                type: 'Point',
+                coordinates: [coordinates[1], coordinates[0]]
+              } as Geometry
+            } as Feature;
           })
-        );
+        } as FeatureCollection;
+      })
+    );
   }
 
   getAuthors() {
