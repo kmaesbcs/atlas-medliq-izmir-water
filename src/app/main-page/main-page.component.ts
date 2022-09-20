f99e3e71d579b1f096572a76b874a874e19f64b3
Adam Kariv <adam.kariv@gmail.com>
2022-09-20 17:37:15 +0300

    Support external navigation

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -190,7 +190,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
   }
 
   redirect(url) {
-    this.ngZone.runTask(() => {
+    this.ngZone.runOutsideAngular(() => {
       window.location.href = window.location.href + `/${url}/`;
     });
     return true;
